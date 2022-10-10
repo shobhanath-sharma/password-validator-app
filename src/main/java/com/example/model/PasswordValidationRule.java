7986d4fa63d3ae6d5e68efb9bcc9faf39f4842eb
shobhanath.sharma <shobhanath.sharma@hotmail.com>
2022-10-10 16:57:14 +0530

    - Renamed model class and enums - Removed sample test class created initially - Written test cases for model

--- src/main/java/com/example/model/PasswordValidationRule.java
+++ src/main/java/com/example/model/PasswordValidationRule.java
@@ -1,11 +1,11 @@
 package com.example.model;
 
-public enum PasswordValidationModel {
+public enum PasswordValidationRule {
     PASSWORD_AT_LEAST_8_CHARS(1, "password should be larger than 8 chars", "^\\w{8,}$"),
     PASSWORD_NON_NULL(2, "password should not be null", "^\\w{1,}$"),
     PASSWORD_WITH_UPPER_CASE_LETTER(3, "password should have one uppercase letter at least", ".*[A-Z].*"),
     PASSWORD_WITH_LOWER_CASE_LETTER(4, "password should have one lowercase letter at least", ".*[a-z].*"),
-    PASSWORD_WITH_NUMBER(5, "password should have one number at least", "(.)*(\\d)(.)*");
+    PASSWORD_WITH_NUMBERIC(5, "password should have one number at least", "(.)*(\\d)(.)*");
 
     public int getId() {
         return id;
@@ -23,7 +23,7 @@ public enum PasswordValidationModel {
     private final String ruleDescription;
     private final String regex;
 
-    PasswordValidationModel(int id, String ruleDescription, String regex) {
+    PasswordValidationRule(int id, String ruleDescription, String regex) {
         this.id = id;
         this.ruleDescription = ruleDescription;
         this.regex = regex;
---
+++ src/test/java/com/example/model/PasswordValidationRuleTest.java
@@ -0,0 +1,76 @@
+package com.example.model;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import static com.example.model.PasswordValidationRule.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class PasswordValidationRuleTest {
+
+    @Test
+    public void shouldCountNumberOfValidationRulesAvailable() {
+        assertThat(PasswordValidationRule.values()).isNotEmpty();
+    }
+
+    @ParameterizedTest
+    @EnumSource(PasswordValidationRule.class)
+    void shouldTestPasswordRuleDescriptionAndRegexAssociated(PasswordValidationRule passwordValidationRule) {
+        switch (passwordValidationRule) {
+            case PASSWORD_AT_LEAST_8_CHARS:
+                assertThat(passwordValidationRule.getRuleDescription()).isEqualTo("password should be larger than 8 chars");
+                assertThat(passwordValidationRule.getRegex()).isEqualTo("^\\w{8,}$");
+                break;
+            case PASSWORD_NON_NULL:
+                assertThat(passwordValidationRule.getRuleDescription()).isEqualTo("password should not be null");
+                assertThat(passwordValidationRule.getRegex()).isEqualTo("^\\w{1,}$");
+                break;
+            case PASSWORD_WITH_UPPER_CASE_LETTER:
+                assertThat(passwordValidationRule.getRuleDescription()).isEqualTo("password should have one uppercase letter at least");
+                assertThat(passwordValidationRule.getRegex()).isEqualTo(".*[A-Z].*");
+                break;
+            case PASSWORD_WITH_LOWER_CASE_LETTER:
+                assertThat(passwordValidationRule.getRuleDescription()).isEqualTo("password should have one lowercase letter at least");
+                assertThat(passwordValidationRule.getRegex()).isEqualTo(".*[a-z].*");
+                break;
+            case PASSWORD_WITH_NUMBERIC:
+                assertThat(passwordValidationRule.getRuleDescription()).isEqualTo("password should have one number at least");
+                assertThat(passwordValidationRule.getRegex()).isEqualTo("(.)*(\\d)(.)*");
+                break;
+            default:
+                fail("Unknown enums and not yet declared in test class");
+        }
+    }
+
+    @Test
+    public void shouldTestPasswordNotNull() {
+        PasswordValidationRule passwordValidationRule = PASSWORD_NON_NULL;
+        assertThat(passwordValidationRule.isValidPassword("A")).isTrue();
+    }
+
+    @Test
+    public void shouldTestPasswordHaveOneUpperCaseLetterAtLeast() {
+        PasswordValidationRule passwordValidationRule = PASSWORD_WITH_UPPER_CASE_LETTER;
+        assertThat(passwordValidationRule.isValidPassword("A")).isTrue();
+    }
+
+    @Test
+    public void shouldTestPasswordHaveOneLowerCaseLetterAtLeast() {
+        PasswordValidationRule passwordValidationRule = PASSWORD_WITH_LOWER_CASE_LETTER;
+        assertThat(passwordValidationRule.isValidPassword("a")).isTrue();
+    }
+
+    @Test
+    public void shouldTestPasswordHaveOneNumericValueAtLeast() {
+        PasswordValidationRule passwordValidationRule = PASSWORD_WITH_NUMBERIC;
+        assertThat(passwordValidationRule.isValidPassword("A1b")).isTrue();
+    }
+
+    @Test
+    public void shouldTestPasswordLargerThan8Chars() {
+        PasswordValidationRule passwordValidationRule = PASSWORD_AT_LEAST_8_CHARS;
+        assertThat(passwordValidationRule.isValidPassword("abcdefgh")).isTrue();
+    }
+}
