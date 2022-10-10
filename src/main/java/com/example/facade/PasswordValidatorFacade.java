6f51ae45b5e501ec525d46a8160fbd1491a2310d
shobhanath.sharma <shobhanath.sharma@hotmail.com>
2022-10-10 18:10:54 +0530

    - Refactored test cases - Updated method in facade method

--- src/main/java/com/example/facade/PasswordValidatorFacade.java
+++ src/main/java/com/example/facade/PasswordValidatorFacade.java
@@ -1,14 +1,39 @@
 package com.example.facade;
 
 import com.example.exception.PasswordValidationException;
+import com.example.service.PasswordValidationService;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class PasswordValidatorFacade {
 
+    private final PasswordValidationService passwordValidationService;
+    private final int numberOfRulesApplied;
+    private List<Integer> mandatoryRuleIdList;
+
+    public PasswordValidatorFacade(final PasswordValidationService passwordValidationService,
+                                   final int numberOfRulesApplied,
+                                   final List<Integer> mandatoryRuleIdList) {
+        this.passwordValidationService = passwordValidationService;
+        this.numberOfRulesApplied = numberOfRulesApplied;
+        this.mandatoryRuleIdList = mandatoryRuleIdList;
+    }
+
+    public PasswordValidatorFacade(final PasswordValidationService passwordValidationService) {
+        this.passwordValidationService = passwordValidationService;
+        this.numberOfRulesApplied = 3;
+        this.mandatoryRuleIdList = Collections.singletonList(4);
+        ;
+    }
+
     public boolean isValid(String password) {
-        if (null != password && password.length() > 0) {
-            return true;
-        } else {
-            throw new PasswordValidationException("Password validation failed as it does not meet password validation rules");
+        List<PasswordValidationException> passwordValidationExceptions = new ArrayList<>();
+        boolean result = passwordValidationService.isValid(password, numberOfRulesApplied, mandatoryRuleIdList, passwordValidationExceptions);
+        if (!result && !passwordValidationExceptions.isEmpty()) {
+            throw passwordValidationExceptions.get(0);
         }
+        return result;
     }
 }
--- src/test/java/com/example/facade/PasswordValidatorFacadeTest.java
+++ src/test/java/com/example/facade/PasswordValidatorFacadeTest.java
@@ -1,6 +1,8 @@
 package com.example.facade;
 
 import com.example.exception.PasswordValidationException;
+import com.example.service.PasswordValidationService;
+import com.example.service.impl.PasswordValidationServiceImpl;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -10,17 +12,21 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PasswordValidatorFacadeTest {
 
-    private static PasswordValidatorFacade validatorFacadeTest;
+    private static PasswordValidatorFacade passwordValidatorFacade;
+    ;
 
     @BeforeAll
     public static void initBeforeAll() {
-        validatorFacadeTest = new PasswordValidatorFacade();
+        PasswordValidationService passwordValidationService = new PasswordValidationServiceImpl();
+        passwordValidatorFacade = new PasswordValidatorFacade(passwordValidationService);
+        assertThat(passwordValidationService).isNotNull();
+        assertThat(passwordValidatorFacade).isNotNull();
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"Abcdefgh", "ABCDEFGh", "ABC EFGh", "abcdefgh", "abc efgh", "1234567h", "1234 67h", "Abcd1234", "Ab", "A@#a12"})
     public void shouldValidatePasswordWithCorrectInput(String inputString) {
-        boolean result = validatorFacadeTest.isValid(inputString);
+        boolean result = passwordValidatorFacade.isValid(inputString);
         assertThat(result).isTrue();
     }
 
@@ -28,7 +34,7 @@ public class PasswordValidatorFacadeTest {
     @ValueSource(strings = {"", "ABCDEFGH", "ABC EFG", "abcd", "12345678", "1234 67", "AB1234", "@!#$%^"})
     public void shouldNotValidatePasswordWithWrongInput(String inputString) {
         assertThatThrownBy(() -> {
-            validatorFacadeTest.isValid(inputString);
+            passwordValidatorFacade.isValid(inputString);
         }).isInstanceOf(PasswordValidationException.class)
                 .hasMessageContaining("Password validation failed as it does not meet password validation rules");
     }
