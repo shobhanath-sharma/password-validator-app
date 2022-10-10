d5c53ed7bb8b1a0b73b188af9f1251a678a50d34
shobhanath.sharma <shobhanath.sharma@hotmail.com>
2022-10-10 17:42:38 +0530

    Refactored existing code and extracted in private method

--- src/main/java/com/example/service/impl/PasswordValidationServiceImpl.java
+++ src/main/java/com/example/service/impl/PasswordValidationServiceImpl.java
@@ -9,26 +9,55 @@ import java.util.stream.Stream;
 public class PasswordValidationServiceImpl implements PasswordValidationService {
     @Override
     public boolean isValid(String password, int numberOfRulesApplied, List<Integer> mandatoryRuleId) {
+        PasswordValidationRule[] allPasswordRules = PasswordValidationRule.values();
         if (numberOfRulesApplied == 0) {
             return true;
         }
-        //TODO: Need to extract and do some cleanup activity here
         //Calculate and consider both numberOfRulesApplied and mandatoryRuleId
-        if (mandatoryRuleId != null && !mandatoryRuleId.isEmpty() && numberOfRulesApplied > mandatoryRuleId.size()) {
+        if (mandatoryRuleId != null &&
+                !mandatoryRuleId.isEmpty() &&
+                numberOfRulesApplied > mandatoryRuleId.size()) {
             numberOfRulesApplied = (numberOfRulesApplied - mandatoryRuleId.size());
-            long mandatoryPasswordValidatedCount = mandatoryRuleId.stream().filter(ruleId -> Stream.of(PasswordValidationRule.values()).anyMatch(rule -> (ruleId == rule.getId()) && rule.isValidPassword(password))).count();
-            long passwordValidatedCount = Stream.of(PasswordValidationRule.values()).filter(passwordValidationRule -> !mandatoryRuleId.contains(passwordValidationRule.getId()) && passwordValidationRule.isValidPassword(password)).count();
-            return (mandatoryPasswordValidatedCount >= mandatoryRuleId.size() && passwordValidatedCount >= numberOfRulesApplied);
+            return applyValidationWithNoOfRulesSpecifiedAndMandatoryRules(password, numberOfRulesApplied, mandatoryRuleId, allPasswordRules);
         }
         // Consider only mandatoryRuleId
-        else if (mandatoryRuleId != null && !mandatoryRuleId.isEmpty() && numberOfRulesApplied <= mandatoryRuleId.size()) {
-            long passwordValidatedCount = mandatoryRuleId.stream().filter(ruleId -> Stream.of(PasswordValidationRule.values()).anyMatch(rule -> (ruleId == rule.getId()) && rule.isValidPassword(password))).count();
-            return passwordValidatedCount >= mandatoryRuleId.size();
+        else if (mandatoryRuleId != null && !mandatoryRuleId.isEmpty()) {
+            return applyValidationWithOnlyMandatoryRules(password, mandatoryRuleId, allPasswordRules);
         }
         //consider only numberOfRulesApplied
         else {
-            long passwordValidatedCount = Stream.of(PasswordValidationRule.values()).filter(passwordValidationRule -> passwordValidationRule.isValidPassword(password)).count();
-            return passwordValidatedCount >= numberOfRulesApplied;
+            return applyValidationWithOnlyNoOfRulesSpecified(password, numberOfRulesApplied, allPasswordRules);
         }
+    }
+
+    private static boolean applyValidationWithOnlyNoOfRulesSpecified(String password,
+                                                                     int numberOfRulesApplied,
+                                                                     PasswordValidationRule[] allPasswordRules) {
+        long passwordValidatedCount = Stream.of(allPasswordRules).
+                filter(passwordValidationRule -> passwordValidationRule.isValidPassword(password)).
+                count();
+        return (passwordValidatedCount >= numberOfRulesApplied);
+    }
+
+    private static boolean applyValidationWithOnlyMandatoryRules(String password,
+                                                                 List<Integer> mandatoryRuleId,
+                                                                 PasswordValidationRule[] allPasswordRules) {
+        long passwordValidatedCount = mandatoryRuleId.stream().
+                filter(ruleId -> Stream.of(allPasswordRules).anyMatch(rule -> (ruleId == rule.getId()) && rule.isValidPassword(password))).
+                count();
+        return (passwordValidatedCount >= mandatoryRuleId.size());
+    }
+
+    private static boolean applyValidationWithNoOfRulesSpecifiedAndMandatoryRules(String password,
+                                                                                  int numberOfRulesApplied,
+                                                                                  List<Integer> mandatoryRuleId,
+                                                                                  PasswordValidationRule[] allPasswordRules) {
+        long mandatoryPasswordValidatedCount = mandatoryRuleId.stream().
+                filter(ruleId -> Stream.of(allPasswordRules).anyMatch(rule -> (ruleId == rule.getId()) && rule.isValidPassword(password))).
+                count();
+        long passwordValidatedCount = Stream.of(allPasswordRules).
+                filter(passwordValidationRule -> !mandatoryRuleId.contains(passwordValidationRule.getId()) && passwordValidationRule.isValidPassword(password)).
+                count();
+        return (mandatoryPasswordValidatedCount >= mandatoryRuleId.size() && passwordValidatedCount >= numberOfRulesApplied);
     }
 }
