34fd8590c878e7af98b293782c5d5c851e81c489
shobhanath.sharma <shobhanath.sharma@hotmail.com>
2022-10-10 17:17:24 +0530

    Written test case for service class

---
+++ src/test/java/com/example/PasswordValidationServiceTest.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PasswordValidationServiceTest {
+    private static PasswordValidationService passwordValidationService;
+
+    @BeforeAll
+    public static void beforeAll(){
+        passwordValidationService = new PasswordValidationService();
+    }
+
+    @Test
+    public void shouldPasswordValidWithGivenNoOfRules(){
+       boolean result =  passwordValidationService.isValid("password", 3);
+       assertThat(result).isTrue();
+    }
+}
